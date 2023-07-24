322e057d3eb52d4646b6ee03e4c2c30c84eda1e5
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-24 13:10:12 +0200

    Tests for Game component

---
+++ src/tests/Game.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Game from '../components/Game';
+
+
+test('renders without errors', () => {
+    render(<Game />);
+});
+
+test('used numbers container', () => {
+    const { getByTestId } = render(<Game />);
+
+    const usedContainer = getByTestId('used-numbers');
+
+    expect(usedContainer).toBeInTheDocument();
+});
+
+test('available numbers container', () => {
+    const { getByTestId } = render(<Game />);
+
+    const availableNumbers = getByTestId('available-numbers');
+
+    expect(availableNumbers).toBeInTheDocument();
+});
+
+test('candidate numbers', () => {
+    const { queryAllByTestId } = render(<Game />);
+    const candidates = queryAllByTestId('candidate-number');
+    expect(candidates).toHaveLength(0);
+});
+
+
