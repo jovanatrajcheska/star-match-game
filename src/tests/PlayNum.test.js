891ced9ebb9059a7262c02c7d31758b6e63db846
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-24 12:31:42 +0200

    Changes made for PlayNum.test.js

--- src/tests/PlayNum.test.js
+++ src/tests/PlayNum.test.js
@@ -5,17 +5,17 @@ import '@testing-library/jest-dom/extend-expect';
 import PlayNum from '../components/PlayNum';
 
 test('renders without errors', () => {
-  render(<PlayNum />);
+    render(<PlayNum />);
 });
 
-test('Number click - available case', () =>{
+test('Number click - available case', () => {
     const numberClick = jest.fn();
 
-    const { getByText } = render(<PlayNum number = {3} status = "available" onClick = {numberClick} />);
+    const { getByText } = render(<PlayNum number={3} status="available" onClick={numberClick} />);
 
     const numberToClick = getByText('3');
     fireEvent.click(numberToClick);
-    
+
     expect(numberClick).toHaveBeenCalledTimes(1);
     expect(numberClick).toHaveBeenLastCalledWith(3, 'available');
 
@@ -24,13 +24,13 @@ test('Number click - available case', () =>{
 
 test('Number click - used case', () => {
     const numberClick = jest.fn();
-  
+
     const { getByText } = render(<PlayNum number={4} status="used" onClick={numberClick} />);
-  
+
     const numberUsedButton = getByText('4');
-    expect(numberUsedButton).toHaveClass('numberUsed');
+    expect(numberUsedButton).toHaveClass('used');
 
     expect(getComputedStyle(numberUsedButton).backgroundColor).toBe('lightgreen');
-  });
-  
-  
+});
+
+
