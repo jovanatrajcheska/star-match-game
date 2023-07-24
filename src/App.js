15dea3dbbbb9b6ff02c9463b5f0e47888c938249
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-25 00:02:58 +0200

    Return Game.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import StarMatch from './components/StarMatch';
+import Game from './components/Game';
 function App() {
   return (
     <div className="App">
-      <StarMatch />
+      <Game />
     </div>
   );
 }
