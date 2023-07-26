156aca090ac5f86b56a18aa474c3cb0659aa88f8
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-26 02:19:20 +0200

    Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
 const Button = ({ onClick, className, style, children, ...addParams }) => (
-
   <button onClick={onClick} className={className} style={style} {...addParams}>
-
     {children}
-
   </button>
 );
 
