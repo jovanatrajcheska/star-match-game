0c75376ef51dc45a5b3049d6059e878c5c7620b6
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-26 14:59:18 +0200

    changes for PlayAgain component

--- src/components/PlayAgain.js
+++ src/components/PlayAgain.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Button from './Button';
 import Message from './Message';
 
-const PlayAgain = ({ gameStatus, onClick }) => (
-  <div className="game-done">
-    <Message gameStatus={gameStatus} />
-    <Button onClick={onClick}>Play Again</Button>
-  </div>
-);
+const PlayAgain = ({ gameStatus, onClick }) => {
+  
+  const messageToDisplay = gameStatus === 'lost' ? 'Game Over' : 'Nice';
+  const messageColor = gameStatus === 'lost' ? 'red' : 'green';
+
+  return (
+    <div className="game-done">
+      <Message text = { messageToDisplay } color = { messageColor } />
+      <Button onClick = { onClick }> Play Again </Button>
+    </div>
+  );
+};
 
 export default PlayAgain;
