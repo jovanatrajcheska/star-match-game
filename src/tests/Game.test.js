d23d26b9713c4654c5cc3bb7d734d8018cfb9240
jovanatrajcheska <trajceskaj55@gmail.com>
2023-08-07 09:57:34 +0200

    Changing tests for Game component

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Game from '../components/Game';
+import Game from '../screens/Game';
 
-
-test('renders without errors', () => {
-    render(<Game />);
+test("renders without errors", () => {
+  render(<Game />);
 });
 
-test('used numbers container', () => {
-    const { getByTestId } = render(<Game />);
+test("used numbers container", () => {
+  const { getByTestId } = render(<Game />);
 
-    const usedContainer = getByTestId('used-numbers');
-
-    expect(usedContainer).toBeInTheDocument();
+  const usedContainer = getByTestId("used-numbers");
+  expect(usedContainer).toBeInTheDocument();
 });
 
-test('available numbers container', () => {
-    const { getByTestId } = render(<Game />);
+test("available numbers container", () => {
+  const { getByTestId } = render(<Game />);
 
-    const availableNumbers = getByTestId('available-numbers');
-
-    expect(availableNumbers).toBeInTheDocument();
+  const availableNumbers = getByTestId("available-numbers");
+  expect(availableNumbers).toBeInTheDocument();
 });
 
-test('candidate numbers', () => {
-    const { queryAllByTestId } = render(<Game />);
-    const candidates = queryAllByTestId('candidate-number');
-    expect(candidates).toHaveLength(0);
+test("candidate numbers", () => {
+  const { queryAllByTestId } = render(<Game />);
+
+  const candidates = queryAllByTestId("candidate-number");
+  expect(candidates).toHaveLength(0);
 });
-
-
